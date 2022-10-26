265103e580d2f29f0b1eca7e59e6d0f4c26017be
rafaa1999 <raferhouma10@gmail.com>
2022-10-26 22:27:57 +0100

    design navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import Logo from '../img/mario_avatar.jpg'
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className='container'>
+            <div className='logo'>
+                <img src={Logo} alt="mario" />
+            </div>
+            <div className='links'>
+                <Link className='link' to="/?cat=art"> 
+                    <h6>ART</h6> 
+                </Link>
+                <Link className='link' to="/?cat=art"> 
+                    <h6>SCIENCE</h6> 
+                </Link>
+                <Link className='link' to="/?cat=art"> 
+                    <h6>TECHNOLOGY</h6> 
+                </Link>
+                <Link className='link' to="/?cat=art"> 
+                    <h6>CINEMA</h6> 
+                </Link>
+                <Link className='link' to="/?cat=art"> 
+                    <h6>DESIGN</h6> 
+                </Link>
+                <Link className='link' to="/?cat=art"> 
+                    <h6>FOOD</h6> 
+                </Link>
+                <span>John</span>
+                <span>Logout</span>
+                <span className='write'>
+                    <Link className='link' to='/write'>Write</Link>
+                </span>
+            </div>
+        </div>
+    </div>
   )
 }
