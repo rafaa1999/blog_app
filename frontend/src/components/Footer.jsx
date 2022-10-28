262c0cb58c0475fa4cc705d58b8ec8fe7006c235
rafaa1999 <raferhouma10@gmail.com>
2022-10-28 21:59:38 +0100

    design footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-
+import Logo from '../img/mario_avatar.jpg'
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <img src={Logo} alt="mario" />
+      <span>i love rafaa</span>
+    </footer>
   )
 }
