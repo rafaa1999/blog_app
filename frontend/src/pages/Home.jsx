97e62970119ecf2bad4f9d36b3bc512460f9e6f3
rafaa1999 <raferhouma10@gmail.com>
2022-10-28 22:35:11 +0100

    design the home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,47 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Home() {
+export default  function Home() {
+
+    const posts=[
+      {
+        id:1,
+        title:"lorem dededededede",
+        desc:"lorem deeeeee",
+        img:"https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
+      },
+      {
+        id:2,
+        title:"lorem dededededede",
+        desc:"lorem deeeeee",
+        img:"https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
+      },
+      {
+        id:3,
+        title:"lorem dededededede",
+        desc:"lorem deeeeee",
+        img:"https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
+      },
+    ]
+
   return (
-    <div>Home</div>
+    <div className='home' >
+      <div className="posts">
+        {posts.map(post=>(
+          <div className="post" key={post.id}>
+            <div className="img">
+              <img src={post.img} alt="" />
+            </div>
+            <div className="content">
+              <Link className='link' to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
+              </Link>
+                <p>{post.desc}</p>
+                <button>read more</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
